e44f173cdc974a5b1079eb3848bc68401f14e81b
jslee <jslee7120@naver.com>
2019-05-01 15:34:46 +0900

    전에 있던거중에 필요 없는것 도 지움

--- app/src/main/java/yaksok/dodream/com/yaksok_refactoring/view/Main/IPresenterToView_Main.java
+++
@@ -1,6 +0,0 @@
-package yaksok.dodream.com.yaksok_refactoring.view.Main;
-
-public interface IPresenterToView_Main {
-
-    void getAutoInfo(boolean auto);
-}
---
+++ app/src/main/java/yaksok/dodream/com/yaksok_refactoring/view/Main/Main_PresenterToView.java
@@ -0,0 +1,7 @@
+package yaksok.dodream.com.yaksok_refactoring.view.Main;
+
+public interface Main_PresenterToView {
+    void onMyNearPillResponce(boolean MyNearPillResponse);
+    void MyNearTime(int nearTime_sec, boolean pillTime_day);
+
+}
