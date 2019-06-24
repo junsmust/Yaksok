6797bbae2659d7f9b8ac9d2901c984ad12966046
jslee <jslee7120@naver.com>
2019-06-24 15:02:37 +0900

    no message

--- app/src/main/java/yaksok/dodream/com/yaksok_refactoring/view/MyPill/Dialog/Custom_pill_delete_Dialog.java
+++ app/src/main/java/yaksok/dodream/com/yaksok_refactoring/view/MyPill/Dialog/Custom_pill_delete_Dialog.java
@@ -1,12 +1,14 @@
-package yaksok.dodream.com.yaksok_refactoring;
+package yaksok.dodream.com.yaksok_refactoring.view.MyPill.Dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -15,22 +17,31 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Custom_pill_delete_Dialog{
+import yaksok.dodream.com.yaksok_refactoring.R;
+import yaksok.dodream.com.yaksok_refactoring.presenter.MyPill.Dialog.Presenter_Dialog;
+import yaksok.dodream.com.yaksok_refactoring.view.MyPill.MyPill_activity;
+
+public class Custom_pill_delete_Dialog implements Dialog_PresenterToView{
     Context context;
     String pill_name,pill_data;
     int pillNo;
+    Presenter_Dialog presenter_dialog;
+    MyPill_activity myPill_activity;
 
     public Custom_pill_delete_Dialog(Context context){
         this.context = context;
 
     }
 
-    public void callFunction(String name, String data, int pillNo) {
+
+    public void callFunction(String name, String data, final int pillNo) {
         this.pill_name = name;
         this.pill_data = data;
         this.pillNo = pillNo;
 
         final Dialog dlg = new Dialog(context);
+
+        presenter_dialog = new Presenter_Dialog(this);
 
 
         // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
@@ -55,10 +66,13 @@ public class Custom_pill_delete_Dialog{
         name1.setText("약 이름 : "+pill_name);
         date.setText("등록날짜 : "+pill_data);
 
+        myPill_activity = new MyPill_activity();
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // 커스텀 다이얼로그를 종료한다.
+                Log.d("test...","삭제 눌림");
+                presenter_dialog.myPillDelete(pillNo);
                 dlg.dismiss();
             }
         });
@@ -72,4 +86,11 @@ public class Custom_pill_delete_Dialog{
         });
     }
 
+    @Override
+    public void onMyPillDeleteRespoce(boolean MyPillResponse) {
+        if(MyPillResponse){
+
+        }
+
+    }
 }
